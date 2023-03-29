853d3d966cf4ce95b859b7cd1bf9ff393d262acd
sambeck87 <sandro_mipb@hotmail.com>
2023-03-29 16:22:04 -0600

    Validate title, comments, and likes from Post Validate name and posts from User

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,8 @@ class Post < ApplicationRecord
   def update_post_counter
     author.increment!(:posts_counter)
   end
+
+  validates :title, presence: true, length: { maximun: 250, too_long: '%<count>s characters is the maximum allowed' }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
