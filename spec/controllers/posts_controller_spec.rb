4278bfb2d87dfc56727dc991c3454999cfb4963b
sambeck87 <sandro_mipb@hotmail.com>
2023-04-03 17:44:13 -0600

    Change to type:request

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
-  render_views
-
+RSpec.describe PostsController, type: :request do
   describe 'GET /show' do
     context 'posts should render correctly' do
       it 'renders a successful response' do
-        get :index, params: { user_id: 3 }
+        get '/users/3/posts'
         expect(response).to be_successful
       end
 
       it 'renders the index template' do
-        user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.',
-                           posts_counter: 0)
-        get :index, params: { user_id: user.id }
+        user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
+        get "/users/#{user.id}/posts"
         expect(response).to render_template(:index)
       end
 
       it 'renders the index template' do
-        get :index, params: { user_id: 5 }
+        get '/users/5/posts'
         expect(response.body).to include('List of posts')
       end
 
       it 'renders the show template' do
-        get :show, params: { user_id: 5, id: 8 }
+        get '/users/5/posts/8'
         expect(response).to render_template(:show)
       end
 
       it 'renders the show template' do
-        get :show, params: { user_id: 1, id: 1 }
+        get '/users/1/posts/1'
         expect(response.body).to include("Post's information")
       end
     end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-  render_views
+RSpec.describe "Users", type: :request do
 
-  describe 'GET /show' do
-    context 'users should render correctly' do
-      it 'renders a successful response' do
-        get :index
+  describe "GET /users" do
+    context "users should render correctly" do
+      it "renders a successful response" do
+        get users_path
         expect(response).to be_successful
       end
 
-      it 'renders the correct template' do
-        get :index
+      it "renders the correct template" do
+        get users_path
         expect(response).to render_template(:index)
       end
 
-      it 'renders the correct template' do
-        get :show, params: { id: 4 }
+      it "renders the correct template" do
+        get user_path(id: 4)
         expect(response).to render_template(:show)
       end
 
-      it 'renders the view with the correct placeholder text' do
-        get :index
-        expect(response.body).to include('List of users')
+      it "renders the view with the correct placeholder text" do
+        get users_path
+        expect(response.body).to include("List of users")
       end
 
-      it 'renders the view with the correct placeholder text' do
-        get :show, params: { id: 4 }
+      it "renders the view with the correct placeholder text" do
+        get user_path(id: 4)
         expect(response.body).to include("User's information")
       end
     end
