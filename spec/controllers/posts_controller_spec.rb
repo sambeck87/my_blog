8e8cb16ce8dd311457f72ecbad57336d25d9e153
sambeck87 <sandro_mipb@hotmail.com>
2023-04-04 16:58:38 -0600

    Fix style linters

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,14 +3,20 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :request do
   describe 'GET /show' do
     context 'posts should render correctly' do
+      user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                         bio: 'Teacher from Poland.', posts_counter: 0)
+      post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                         likes_counter: 0)
       it 'renders a successful response' do
-        get '/users/3/posts'
+        get "/users/#{user.id}/posts"
         expect(response).to be_successful
       end
 
       it 'renders the index template' do
         user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                            bio: 'Teacher from Poland.', posts_counter: 0)
+        post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                           likes_counter: 0)
         get "/users/#{user.id}/posts"
         expect(response).to render_template(:index)
       end
@@ -18,10 +24,14 @@ RSpec.describe PostsController, type: :request do
       it 'renders the index template' do
         user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                            bio: 'Teacher from Poland.', posts_counter: 0)
+        post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                           likes_counter: 0)
         get "/users/#{user.id}/posts"
-        expect(response.body).to include('List of posts')
+        expect(response.body).to include('Lilly')
       end
+    end
 
+    context 'posts should contain the correct information' do
       it 'renders the show template' do
         user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                            bio: 'Teacher from Poland.', posts_counter: 0)
@@ -37,7 +47,7 @@ RSpec.describe PostsController, type: :request do
         post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
                            likes_counter: 0)
         get "/users/#{user.id}/posts/#{post.id}"
-        expect(response.body).to include("Post's information")
+        expect(response.body).to include('This is my first post')
       end
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,15 +21,17 @@ RSpec.describe 'Users', type: :request do
       end
 
       it 'renders the view with the correct placeholder text' do
+        User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                    bio: 'Teacher from Poland.', posts_counter: 0)
         get users_path
-        expect(response.body).to include('List of users')
+        expect(response.body).to include('Lilly')
       end
 
       it 'renders the view with the correct placeholder text' do
         user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                            bio: 'Teacher from Poland.', posts_counter: 0)
         get user_path(id: user.id)
-        expect(response.body).to include("User's information")
+        expect(response.body).to include('Lilly')
       end
     end
   end
