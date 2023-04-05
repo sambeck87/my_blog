73fef1075bc0138c1e51822084af8e8b3ff655d7
sambeck87 <sandro_mipb@hotmail.com>
2023-04-05 14:48:47 -0600

    Implement post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,26 @@ class PostsController < ApplicationController
     @comments = @post.comments
     render :show
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = ApplicationController.current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_posts_path(ApplicationController.current_user), notice: "El post se creó correctamente."
+    else
+     redirect_to user_posts_path, notice: "El post NO se creó correctamente."
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
