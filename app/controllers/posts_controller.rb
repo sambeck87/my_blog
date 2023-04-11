d99d6af0756b11766fcf9a59681749ddee715c11
sambeck87 <sandro_mipb@hotmail.com>
2023-04-10 19:57:18 -0600

    include the comments when gets the users posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.eager_load(posts: [:comments]).find(params[:user_id])
     @posts = @user.posts
     render :index
   end
 
   def show
-    @post = Post.includes(comments: [:author]).find(params[:id])
+    @post = Post.eager_load(comments: [:author]).find(params[:id])
     @comments = @post.comments
     render :show
   end
